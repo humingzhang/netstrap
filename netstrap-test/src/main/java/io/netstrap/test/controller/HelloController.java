e7530ecb401b54e3741f9e6a3a7cf37be9040096
mr.zhang <408858889@qq.com>
2018-12-12 20:49:50 +0800

    解决keepalive 链接不关闭bug

--- netstrap-test/src/main/java/io/netstrap/test/controller/HelloController.java
+++ netstrap-test/src/main/java/io/netstrap/test/controller/HelloController.java
@@ -5,15 +5,14 @@ import io.netstrap.core.server.http.datagram.HttpResponse;
 import io.netstrap.core.server.mvc.stereotype.RestController;
 import io.netstrap.core.server.mvc.stereotype.mapping.GetMapping;
 import io.netstrap.core.server.mvc.stereotype.mapping.PostMapping;
+import io.netstrap.core.server.mvc.stereotype.parameter.ContextValue;
 import io.netstrap.core.server.mvc.stereotype.parameter.FormValue;
-import io.netstrap.core.server.mvc.stereotype.parameter.RequestBody;
 import io.netstrap.test.config.WechatConfig;
 import io.netty.handler.codec.http.multipart.MixedFileUpload;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * 控制器示例
@@ -35,21 +34,17 @@ public class HelloController {
     /**
      * 打印字符串
      */
-    @PostMapping("/hello")
-    public String hello(@RequestBody Map map) {
-        map.forEach((key, value) -> {
-            System.out.println(key + "," + value);
-        });
-        return "hello netstrap";
+    @GetMapping("/hello")
+    public String hello(@ContextValue String id) {
+        return "hello netstrap ->" + id;
     }
 
     /**
      * 打印字符串
      */
-    @PostMapping("/hi")
-    public String hi(HttpRequest request, HttpResponse response) {
-        response.setStatus(401);
-        return "hello netstrap";
+    @GetMapping("/hi")
+    public String hi(@ContextValue String id) {
+        return "hello netstrap -> " + id;
     }
 
     /**
