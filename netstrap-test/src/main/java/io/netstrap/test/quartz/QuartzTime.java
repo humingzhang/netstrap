8b1ca20f8349d6cf3c3103d3c53c7d0d75343ce9
mr.zhang <408858889@qq.com>
2018-12-19 17:57:38 +0800

    增加websocket 推送测试

--- netstrap-test/src/main/java/io/netstrap/test/quartz/QuartzTime.java
+++ netstrap-test/src/main/java/io/netstrap/test/quartz/QuartzTime.java
@@ -1,8 +1,11 @@
 package io.netstrap.test.quartz;
 
+import io.netstrap.core.server.websocket.WebSocketGroup;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.nio.channels.Channel;
 
 /**
  * 定时器测试
@@ -16,8 +19,9 @@ public class QuartzTime {
     /**
      * 执行刷新间隔
      */
-    @Scheduled(cron = "0/3 * * * * ?")
+    @Scheduled(cron = "0/1 * * * * ?")
     public void loopSayHello() {
-        //TODO
+        String message = "{\"timestamp\":"+System.currentTimeMillis()+",\"linking\":"+WebSocketGroup.count()+"}";
+        WebSocketGroup.push(message);
     }
 }
