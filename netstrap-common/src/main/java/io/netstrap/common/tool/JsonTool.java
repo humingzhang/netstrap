9ce9730f212b69e4644616e8ee1e8ab9b051ea73
mr.zhang <408858889@qq.com>
2019-01-15 16:25:36 +0800

    移除FastJson循环引用

--- netstrap-common/src/main/java/io/netstrap/common/tool/JsonTool.java
+++ netstrap-common/src/main/java/io/netstrap/common/tool/JsonTool.java
@@ -2,9 +2,7 @@ package io.netstrap.common.tool;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.sun.xml.internal.ws.api.model.CheckedException;
 
-import java.io.UncheckedIOException;
 import java.util.Map;
 
 /**
